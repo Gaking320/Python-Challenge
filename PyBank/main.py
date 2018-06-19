22722084d2f5afc77ff77b7313b1c9efe937f872
Geoff King <geoffrey_a_king@outlook.com>
2018-06-19 18:09:55 -0400

    W.I.P

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,48 @@
-'PyBank.main.PyBank
+#'PyBank.main.PyBank
+#Objective: Create a python script for analyzing the financial records of the company
+#Data: budget_data.csv - columns: 'date' and 'profit/losses'
+
+#Greatest increase in profits(date and amount over entire period
+#Greatest decrease in losses (date and amount) over entire period
+#Print output to terminal and export a text file
+import os
+import csv
+# Path to collect data from the PyBank folder
+budget_data.csv = os.path.join("../Users/geoff/Documents/Python-Challenge/PyBank", "budget_data.csv")
+
+
+with open ('budget_data.csv',r) as csvfile: #---Read in csv file
+    cvsreader = csv.reader(csvfile, delimiter=",") #---Split the data on commas as iterator
+    csvreader.next() #Skip header row
+    revenue = []
+    date = []
+    rev_change = []
+
+    #Total months in dataset = total number of rows in the csvfile, print total
+    row_count = sum(1 for row in csvreader)
+         
+    #Total net amount of profit/losses over period, print  sum total
+    total = 0
+    for row in csvreader:  
+        total += float(row[2])
+        
+    #Average change in Profit/losses between months over the entire period
+    #Difference between all row of column "Revenue" and found total revnue change. 
+    for i in range(1,len(revenue)):
+        rev_change.append(revenue[i] - revenue[i-1])   
+        avg_rev_change = sum(rev_change)/len(rev_change)
+
+        max_rev_change = max(rev_change) #Max revenue change
+
+        min_rev_change = min(rev_change) #Min revenue change
+
+        max_rev_change_date = str(date[rev_change.index(max(rev_change))])
+        min_rev_change_date = str(date[rev_change.index(min(rev_change))])
+    
+    print("Financial Analysis")
+    print("-----------------------------------")
+    print( "Total Months: " + row_count)
+    print("Total: $"+total)
+    print("Avereage Change: $", round(avg_rev_change))
+    print("Greatest Increase in profits:", max_rev_change_date,"($", max_rev_change,")")
+    print("Greatest Decrease in profits:", min_rev_change_date,"($", min_rev_change,")")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,62 @@
-'PyPoll.main
+#'PyPoll.main
+#Objective: Create a python script for analyzing and calculating votes.
+#Data:  election_data.csv - columns: 'voter ID', 'County','Candidate'
+
+#Total number of votes cast
+#complete list of candidates who received  votes
+#Percentage of votes each candidate  received
+#Total number of votes each candidate received
+#winner of the election based on total votes received
+
+import os
+import csv
+#path to collect data from the PyPoll folder
+election_data.csv = os.path.join("../Users/geoff/Documents/Python-Challenge/PyPoll", election_data.csv)
+
+#Read in csv file
+with open('election_data.csv',r) as csvfile:
+    cvsreader = csv.reader(csvfile, delimiter=",") #---Split the data on commas as iterator
+    cvsreader.next() #---Skip header row
+    
+    #Total number of votes = Total number of rows in csvfile
+    row_count = sum(1 for row in csvreader)
+
+    #Complete list of Candidate names who received votes.
+    candidates_list = [row[2] for row in cvsreader]
+
+    #Counter total number of votes per candidate, where k is the candidate key, and v are the votes per candidate
+    from collections import Counter
+    z = candidates_list
+    Counter_list = Counter(z)
+    for k, v in Counter_list.items():
+        winner = max(Counter_list, key=Counter_list.get) #winner of election based on popular vote
+        y = v * 100 / row_count #calculate the percentage of votes each candidate won
+
+        print("Election Results")
+        print("------------------------")
+        print("Total Votes: " + row_count)
+        print("--------------------------")
+        print(k ":" + y + "%" + "(" + v +")")
+        print("-------------------------")
+        print("Winner: " + winner)
+        print("----------------------------")
+        
+
+
+
+
+
+
+
+
+
+
+    #Define function to get unique values = names
+    def unique_list(candidates_list):
+        output_list[]
+        for word in candidates_list:
+            if word not in output_list:
+                output_list.append(word)
+            print(output_list)
+            return output_list
+    
